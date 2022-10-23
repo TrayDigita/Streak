45f1a8302b5bc4f8bbbe093a2edda8ed322d1504
ArrayIterator <arrayiterator@gmail.com>
2022-10-23 15:26:18 +0000

    Add more global usage

--- app/Source/ACL/UserControl.php
+++ app/Source/ACL/UserControl.php
@@ -1,11 +1,16 @@
 <?php
+/** @noinspection PhpUnused */
 declare(strict_types=1);
 
 namespace TrayDigita\Streak\Source\ACL;
 
+use ReturnTypeWillChange;
 use TrayDigita\Streak\Source\ACL\Interfaces\IdentityInterface;
 use TrayDigita\Streak\Source\Traits\PasswordHashed;
 
+/**
+ * Class helper
+ */
 class UserControl
 {
     use PasswordHashed;
@@ -16,44 +21,63 @@ class UserControl
     protected array $acl = [];
 
     /**
-     * @param int $id
-     * @param string $username
-     * @param string $email
-     * @param string $password
+     * @param int|string|float $id
+     * @param ?string $username
+     * @param ?string $email
+     * @param ?string $password
      */
     public function __construct(
-        protected int $id,
-        protected string $username,
-        protected string $email,
-        protected string $password
+        protected int|string|float $id,
+        protected ?string $username = null,
+        protected ?string $email = null,
+        protected ?string $password = null
     ) {
     }
 
+    /**
+     * Validate password
+     *
+     * @param string $password
+     *
+     * @return bool
+     */
     public function validate(string $password) : bool
     {
-        return self::verifyPassword($password, $this->getPassword());
+        $userPassword = $this->getPassword();
+        if (!$userPassword) {
+            return false;
+        }
+        return self::verifyPassword($password, $userPassword);
     }
 
     /**
-     * @return string
+     * @return float|int|string
      */
-    public function getUsername(): string
+    #[ReturnTypeWillChange] public function getId(): float|int|string
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return ?string
+     */
+    #[ReturnTypeWillChange] public function getUsername() : ?string
     {
         return $this->username;
     }
 
     /**
-     * @return string
+     * @return ?string
      */
-    public function getEmail(): string
+    #[ReturnTypeWillChange] public function getEmail(): ?string
     {
         return $this->email;
     }
 
     /**
-     * @return string
+     * @return ?string
      */
-    public function getPassword(): string
+    #[ReturnTypeWillChange] public function getPassword(): ?string
     {
         return $this->password;
     }
